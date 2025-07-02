c8f259dd017306738e71cf8e04b398a71fd268d3
Henrique Martins <leaomartinshenrique@gmail.com>
2025-07-01 23:24:25 -0300

    implements rail fence cipher
    Signed-off-by: Henrique Martins <leaomartinshenrique@gmail.com>

--- src/main/java/org/hmleao/encryptor/impl/RailFenceCipher.java
+++ src/main/java/org/hmleao/encryptor/impl/RailFenceCipher.java
@@ -5,12 +5,46 @@ import org.hmleao.encryptor.Cipher;
 
 public class RailFenceCipher extends AbstractCipher {
 
+    private final int numOfRails = 3;
+
     public RailFenceCipher(Cipher cipher) {
         super(cipher);
     }
 
     @Override
     protected String doEncryption(String message) {
-        return message + " _rail_fence_ ";
+        // rail fence cipher
+        StringBuilder result = new StringBuilder();
+        char[] messageArray = message.toCharArray();
+        final char[][] railFence = new char[numOfRails][messageArray.length];
+        int railCount = 0;
+        boolean upCount = false; // true for counting upwards, false for downwards
+        // filling the rail fence
+        for (int i = 0; i < messageArray.length; i++) {
+            char currentChar = messageArray[i];
+            if (currentChar == ' ' || currentChar == ',' || currentChar == '.') continue;
+            railFence[railCount][i] = messageArray[i];
+
+            if (railCount == 0) {
+                upCount = false;
+            } else if (railCount == numOfRails - 1) {
+                upCount = true;
+            }
+
+            if (upCount) {
+                railCount--;
+            } else {
+                railCount++;
+            }
+        }
+        // reading from the filled rail fence
+        for (int i = 0; i < numOfRails; i++) {
+            for (int j = 0; j < messageArray.length; j++) {
+                if (railFence[i][j] == '\u0000') continue;
+                result.append(railFence[i][j]);
+            }
+            result.append(' ');
+        }
+        return result.toString();
     }
 }
